67c332df79f90abac8ea363395d73be6e2f37688
homazd <homa.zohdi@hotmail.com>
2025-10-29 10:12:25 +0330

    blocking vs nonBlocking

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,22 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // blocking code 
+  @Get('blocking')
+  blocking() {
+    return this.appService.blocking()
+  }
+
+  // Non blocking code
+  @Get('nonBlocking')
+  async nonBlocking() {
+    return this.appService.nonBlocking()
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,19 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  blocking() {
+    const now = new Date().getTime();
+    while (new Date().getTime() < now + 10000) {
+
+    } return {}
+  }
+
+  async nonBlocking() {
+    return new Promise(async (resolve) => {
+      setTimeout(() => {
+        resolve({});
+      }, 10000)
+    })
+  }
 }
